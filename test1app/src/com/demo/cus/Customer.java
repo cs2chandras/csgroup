76255333df531a3eec38a2973c6c8c0e997e98a5
Chandra <Chandra@DESKTOP-GE161G7>
2020-07-24 20:18:29 +0530

    city and state updated

--- test1app/src/com/demo/cus/Customer.java
+++ test1app/src/com/demo/cus/Customer.java
@@ -6,6 +6,9 @@ public class Customer {
 	private String name;
 	private String mobNo;
 	private String address;
+	private String city;
+	private String state;
+	private String country;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to customers");
