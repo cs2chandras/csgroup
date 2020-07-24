e254208b916efbffd0a912accd86dcad7b9ea12e
Chandra <Chandra@DESKTOP-GE161G7>
2020-07-24 18:44:11 +0530

    sop added

--- test1app/src/com/demo/cus/Customer.java
+++ test1app/src/com/demo/cus/Customer.java
@@ -3,7 +3,7 @@ package com.demo.cus;
 public class Customer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to customers");
 
 	}
 
