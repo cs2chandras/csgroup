5b5153273362867120721326ec75baf0a92f4d2d
Chandra <Chandra@DESKTOP-GE161G7>
2020-07-24 19:56:32 +0530

    customer fields updated

--- test1app/src/com/demo/cus/Customer.java
+++ test1app/src/com/demo/cus/Customer.java
@@ -2,6 +2,10 @@ package com.demo.cus;
 
 public class Customer {
 
+	private int cusId;
+	private String name;
+	private String mobNo;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to customers");
 
